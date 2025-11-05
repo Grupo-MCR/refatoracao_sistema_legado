3e7746468e79ba103046d1b8b468a3b20d439a92
gustagueimer <gustavomarqueslima@outlook.com>
2025-11-04 21:44:30 -0300

    fix: correção na entidade de venda e implementação de migration

--- sistema_vendas/vendas/models.py
+++ sistema_vendas/vendas/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+from clientes import models as c_models
+from produtos import models as p_models
 
 # Create your models here.
-class Venda(models.Model):
-  id = models.BigAutoField(primary_key = True, null = False, unique = True)
-  cliente_id = models.ForeignKey(Cliente, on_delete=models.CASCADE, null = False,)
-  itemVenda_id = models.ForeignKey(ItemVenda, on_delete=models.CASCADE, null = False,)
-  data_venda = models.DateTimeField(null = False,)
-  total_venda = models.DecimalField(null = False,)
-  observacoes = models.CharField(max_length=255)
-
 class ItemVenda(models.Model):
   id = models.BigAutoField(primary_key = True, null = False, unique = True)
-  produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE, null = False,)
+  produto_id = models.ForeignKey(p_models.Produto, on_delete=models.CASCADE, null = False,)
   quantidade = models.IntegerField
-  subTotal = models.DecimalField(null = False)
+  subTotal = models.DecimalField(null = False, blank= False, max_digits= 10, decimal_places= 2)
+
+class Venda(models.Model):
+  id = models.BigAutoField(primary_key = True, null = False, unique = True)
+  cliente_id = models.ForeignKey(c_models.Cliente, on_delete=models.CASCADE, null = False,)
+  itemVenda_id = models.ForeignKey(ItemVenda, on_delete=models.CASCADE, null = False,)
+  data_venda = models.DateTimeField(null = False, )
+  total_venda = models.DecimalField(null = False, blank= False, max_digits=10, decimal_places= 2)
+  observacoes = models.CharField(max_length=255)
+
+
     
