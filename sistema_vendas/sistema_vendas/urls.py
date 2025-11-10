0e672bc543245bbd06657775c14d229c11b7b173
lluhk <luancarlosmuhlbeier2@gmail.com>
2025-11-10 19:49:40 -0300

    feat: interface de consulta de produtos

--- sistema_vendas/sistema_vendas/urls.py
+++ sistema_vendas/sistema_vendas/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('produtos/', include('produtos.urls')),
     path('admin/', admin.site.urls),
 ]
