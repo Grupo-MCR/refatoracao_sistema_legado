70298866058baf2f1953f6edbcad765dffe16f8f
Ayran04 <ayranbade@gmail.com>
2025-11-12 00:04:42 -0300

    Tela de cadastro de fornecedor

--- sistema_vendas/fornecedores/views.py
+++ sistema_vendas/fornecedores/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.middleware import csrf
+
 
 # Create your views here.
+
+def CadastrarFuncionario(request):
+    if request.method == 'GET':
+        csrf.get_token(request)
+        template = loader.get_template('cadastrarFornecedor.html')
+        return HttpResponse(template.render())
+    else:
+        return HttpResponseBadRequest("método de request inválido :c")
+    
--- sistema_vendas/sistema_vendas/settings.py
+++ sistema_vendas/sistema_vendas/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SistemaVendas',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': '040406',
         'HOST': 'localhost',
         'PORT': '5432'
     }
