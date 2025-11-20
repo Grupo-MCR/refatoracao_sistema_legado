ff79e6e25372bd65cfa2acdaa24068a36a96d792
Arthur Haupt Pacheco <arthurhauptpacheco@gmail.com>
2025-11-20 17:22:39 -0300

    Implementado funcionalidade do pdv e testes do pdv e aproveitado para implementar funcionalidade di pagamento

--- sistema_vendas/vendas/urls.py
+++ sistema_vendas/vendas/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ponto_venda/', views.Venda, name='ponto_venda'),
+    # Tela do Ponto de Vendas
+    path('ponto_venda/', views.Venda_View, name='ponto_venda'),
+    
+    # Tela de Pagamentos
     path('pagamentos/', views.Pagamento, name='pagamentos'),
+    
+    # APIs de busca
+    path('buscar_cliente/', views.buscar_cliente, name='buscar_cliente'),
+    path('buscar_produto/', views.buscar_produto, name='buscar_produto'),
+    
+    # APIs de processamento
+    path('finalizar_venda/', views.finalizar_venda, name='finalizar_venda'),
+    path('processar_pagamento/', views.processar_pagamento, name='processar_pagamento'),
 ]
