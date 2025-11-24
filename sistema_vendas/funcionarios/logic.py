83ee7761b1df8e67fd3dfa9513763cda1c5045ee
gustagueimer <gustavomarqueslima@outlook.com>
2025-11-24 01:04:36 -0300

    feat: adição de função para buscar todos os funcionários cadastrados

--- sistema_vendas/funcionarios/logic.py
+++ sistema_vendas/funcionarios/logic.py
@@ -1,6 +1,14 @@
+from django.core import serializers
 from .models import Funcionario
 
 def buscarFuncionario(id: int):
     funcionario = Funcionario.objects.filter(id=id).values();
     print(funcionario[0]);
-    return funcionario[0]; 
+    return funcionario[0];
+
+def buscarFuncionários():
+    funcionariosList = []
+    funcionarios = Funcionario.objects.all().values()
+    for funcionario in funcionarios:
+        funcionariosList.append(funcionario)
+    return funcionariosList
